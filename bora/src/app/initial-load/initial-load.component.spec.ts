e77a57123ceabd33030c058305f55f5f5af227b9
System Administrator <root@Laianes-MacBook-Pro.local>
2023-03-05 03:00:12 -0300

    initial-setup

---
+++ bora/src/app/initial-load/initial-load.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InitialLoadComponent } from './initial-load.component';
+
+describe('InitialLoadComponent', () => {
+  let component: InitialLoadComponent;
+  let fixture: ComponentFixture<InitialLoadComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ InitialLoadComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(InitialLoadComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ bora/src/app/initial-load/initial-load.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-initial-load',
+  templateUrl: './initial-load.component.html',
+  styleUrls: ['./initial-load.component.scss']
+})
+export class InitialLoadComponent {
+
+}
